b954e2189d57a462802b8b84756d642efae9e3f7
Raman Karseka <ier@live.ru>
2021-03-16 02:15:51 +0300

    feat: solution for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-module.exports = function toReadable (number) {
-  
+const n1 = [
+    '',
+    ' one',
+    ' two',
+    ' three',
+    ' four',
+    ' five',
+    ' six',
+    ' seven',
+    ' eight',
+    ' nine',
+    ' ten',
+    ' eleven',
+    ' twelve',
+    ' thirteen',
+    ' fourteen',
+    ' fifteen',
+    ' sixteen',
+    ' seventeen',
+    ' eighteen',
+    ' nineteen'
+];
+const n2 = [
+    ' zero',
+    ' one',
+    ' twenty',
+    ' thirty',
+    ' forty',
+    ' fifty',
+    ' sixty',
+    ' seventy',
+    ' eighty',
+    ' ninety'
+];
+const n3 = ' hundred';
+const n4 = ['',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+]
+
+module.exports = function toReadable(number) {
+    const r2 = n => n2[Math.trunc(n / 10)] + n1[n % 10];
+    const conv = n => {
+        let str = '';
+        if (n > 99) str = n1[Math.trunc(n / 100)] + ' hundred' + conv(n % 100)
+        else if (n > 19) str = r2(n)
+        else str = n1[n]
+        return str
+    }
+    return number === 0 ? 'zero' : conv(number).substr(1);
 }
